e30203abc9d39159c3e8ecc24723adc3677b3d8c
Ihor Vasylenko <134538606+ihorvasylenko23@users.noreply.github.com>
2023-10-02 12:33:50 +0300

    added car model and repository (#2)
    * added car model and repository

--- src/main/java/com/example/car/sharing/model/Car.java
+++ src/main/java/com/example/car/sharing/model/Car.java
@@ -1,4 +1,48 @@
 package com.example.car.sharing.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE cars SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "cars")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CarType type;
+
+    @Column(nullable = false)
+    private int inventory;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal dailyFee;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    public enum CarType {
+        SEDAN, SUV, HATCHBACK, UNIVERSAL
+    }
 }
