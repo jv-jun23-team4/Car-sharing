bc5271f83c11c6d08bc2fae27a12e1d8691426f8
Denis Moroz <moroz06denis@gmail.com>
2023-10-02 11:32:53 +0200

    added space between fields in payment model

--- src/main/java/com/example/car/sharing/model/Payment.java
+++ src/main/java/com/example/car/sharing/model/Payment.java
@@ -20,18 +20,24 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Type type;
+
     @Column(nullable = false)
     private Long rentalId;
+
     @Column(name = "session_url", nullable = false)
     private URL sessionUrl;
+
     @Column(name = "session_id", nullable = false)
     private String sessionId;
+
     @DecimalMin("0.0")
     @Column(name = "amount_to_pay", nullable = false)
     private BigDecimal amountToPay;
