22fde5b876b19458a059d608cbfdc488600ff351
Iryna Mekh <iryna.mekh@gmail.com>
2023-10-03 20:10:06 +0300

    processed errors in CustomGlobalExceptionHandler

--- src/main/java/com/example/car/sharing/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/car/sharing/exception/CustomGlobalExceptionHandler.java
@@ -2,14 +2,17 @@ package com.example.car.sharing.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.stripe.exception.StripeException;
+import io.jsonwebtoken.JwtException;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -33,6 +36,17 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(responseBody, headers, status);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        ResponseBody responseBody = new ResponseBody(LocalDateTime.now(),
+                HttpStatus.METHOD_NOT_ALLOWED, List.of(ex.getMessage()));
+        return new ResponseEntity<>(responseBody, headers, status);
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(
             EntityNotFoundException ex
@@ -50,6 +64,36 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         ResponseBody responseBody = new ResponseBody(LocalDateTime.now(),
                 HttpStatus.INTERNAL_SERVER_ERROR, List.of(ex.getMessage()));
         return new ResponseEntity<>(responseBody, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RegistrationException.class)
+    public ResponseEntity<Object> handleRegistrationException(
+            RegistrationException ex
+    ) {
+        ResponseBody responseBody = new ResponseBody(LocalDateTime.now(),
+                HttpStatus.CONFLICT, List.of(ex.getMessage()));
+        return new ResponseEntity<>(responseBody,
+                HttpStatusCode.valueOf(HttpStatus.CONFLICT.value()));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<Object> handleJwtException(
+            JwtException ex
+    ) {
+        ResponseBody responseBody = new ResponseBody(LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED, List.of(ex.getMessage()));
+        return new ResponseEntity<>(responseBody,
+                HttpStatusCode.valueOf(HttpStatus.UNAUTHORIZED.value()));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<Object> handleUsernameNotFoundException(
+            UsernameNotFoundException ex
+    ) {
+        ResponseBody responseBody = new ResponseBody(LocalDateTime.now(),
+                HttpStatus.FORBIDDEN, List.of(ex.getMessage()));
+        return new ResponseEntity<>(responseBody,
+                HttpStatusCode.valueOf(HttpStatus.FORBIDDEN.value()));
     }
 
     private String getErrorMessage(ObjectError e) {
