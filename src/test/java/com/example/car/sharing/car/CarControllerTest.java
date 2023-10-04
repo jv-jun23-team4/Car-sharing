3c83ea46e7ff6faa0aca60e16485abfca10fdef6
Denis Moroz <moroz06denis@gmail.com>
2023-10-04 10:50:16 +0200

    added package for car test

--- src/test/java/com/example/car/sharing/car/CarControllerTest.java
+++ src/test/java/com/example/car/sharing/car/CarControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.car.sharing;
+package com.example.car.sharing.car;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
--- src/test/java/com/example/car/sharing/car/CarServiceTest.java
+++ src/test/java/com/example/car/sharing/car/CarServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.car.sharing;
+package com.example.car.sharing.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
