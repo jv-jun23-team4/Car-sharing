c3ec57744eac553d0ab6d93354e8499e15ae5c72
Oleksandr Hatsenko <135050125+ohatsenko@users.noreply.github.com>
2023-10-05 09:42:58 +0300

    [Test]: added tests for user controller (#34)
    Added test for controller and service for user

--- src/main/java/com/example/car/sharing/dto/user/UpdateUserData.java
+++ src/main/java/com/example/car/sharing/dto/user/UpdateUserData.java
@@ -1,9 +1,11 @@
 package com.example.car.sharing.dto.user;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Accessors(chain = true)
 public class UpdateUserData {
     @Length(min = 2, max = 50)
     private String firstName;
---
+++ src/test/resources/database/users/add-customer.sql
@@ -0,0 +1,1 @@
+insert into users (id, email, password, first_name, last_name, role) VALUES (15, 'customer', '$2a$12$.0u6aDCet5OjCry1xm6zpuDHZn.3u97HJ4IazCT2vjmEQ7fdhJu5G', 'James', 'Dean', 'CUSTOMER');
